ba8d1f5ca4bde19ee34ab25e8f698bcd354d902a
hckaplan <hkaplan16@gmail.com>
2017-05-24 07:20:55 -0400

    Add files via upload

--- openFDA_AEs_by_country.R
+++ openFDA_AEs_by_country.R
@@ -67,7 +67,8 @@ for (i in 1:length(countries.results$term))
                countries.results$term[i], "&count=patient.reaction.reactionmeddrapt.exact&limit=1000",
                sep="")
   aes.raw <- GET(url)
-  #throw an error and stop processing if the query does not return a successful status code
+  
+  #check that query was successful
   if (aes.raw$status_code != 200) {stop("Unsuccessful query")}
   
   #convert raw results to characters
@@ -92,9 +93,7 @@ countries.AEs.f <- merge(x=countries.AEs.all, y=countries.AEs.top, by=c("country
 if (nrow(countries.AEs.top) < as.integer(num_countries)*as.integer(num_aes)) {
   for(i in 1:nrow(countries.AEs.f)) {
     if (is.na(countries.AEs.f$count.x[i])) {
-      #query the openFDA database for the missing data point
-      
-      #make version of missing term for query
+      #make version of missing term in format required for query
       queryterm <- gsub(" ", "+", countries.AEs.f$term[i])
       
       #query missing country/event combination
@@ -104,7 +103,7 @@ if (nrow(countries.AEs.top) < as.integer(num_countries)*as.integer(num_aes)) {
                    sep="")
       data.raw <- GET(url)
       
-      #throw an error and stop processing if the query does not return a successful status code
+      #check that query was successful
       if (data.raw$status_code != 200) {stop("Unsuccessful query")}
       
       #convert raw results to characters
@@ -120,7 +119,7 @@ if (nrow(countries.AEs.top) < as.integer(num_countries)*as.integer(num_aes)) {
   }
 }
 
-#make AEs proportional to total AEs to account for differing population sizes and reporting rates
+#make AEs proportional to total AEs to account for differing population sizes and overall reporting rates
 countries.AEs.f$index_aes <- countries.AEs.f$count.x/countries.AEs.f$total
 
 #qc to make sure values are between 0 and 1
@@ -135,6 +134,7 @@ countries.AEs.stats$range <- countries.AEs.stats$max - countries.AEs.stats$min
 countries.AEs.stats[order(-countries.AEs.stats$range), ]
 
 #label countries with full names vs abbreviations
+#get mapping of abbreviation to name
 abbr.raw <- GET("http://data.okfn.org/data/core/country-list/r/data.json")
 
 #check that list grab was successful
@@ -143,6 +143,7 @@ abbr.raw$status_code
 #convert raw results to character fields and isolate results
 abbr.char <- as.data.frame(fromJSON(rawToChar(abbr.raw$content)))
 
+#merge full names with dataset
 countries.AEs.cln <- merge(x=countries.AEs.f, y=abbr.char, by.x="country", by.y="Code", all.x=TRUE)
 
 #update some names for clarity
@@ -155,7 +156,7 @@ ae_highest_range <- countries.AEs.stats[which(countries.AEs.stats$range == max(c
 #get the data for the AE with highest range across countries
 countries.highest.range.ae <- subset(countries.AEs.cln, term==ae_highest_range)
 
-#qc
+#qc row count
 if(nrow(countries.highest.range.ae) != as.integer(num_countries)) {stop("Code error - check subset for AE with highest range")}
 
 #get the countries in order of the highest ranged AE
@@ -183,6 +184,7 @@ world$region <- gsub("UK", "United Kingdom", world$region)
 world.aes <- merge(x=world, y=countries.highest.range.ae,by.x="region", by.y="Name", all.x=TRUE)
 world.aes <- world.aes[order(world.aes$order), ]
 
+#plot heatmap of AE with highest range across countries by country
 ggplot(world.aes, aes(long, lat)) + 
   geom_polygon(aes(fill=index_aes, group=group),colour="black",size=0.1) +
   coord_equal() + 
@@ -198,11 +200,18 @@ ggplot(world.aes, aes(long, lat)) +
 #transpose data from long to wide
 countries.AEs.trans <- reshape(countries.AEs.cln[, c("Name", "term", "index_aes")], idvar = "Name", timevar = "term", direction = "wide")
 
+#change country from first column to rowname for easy processing
 rownames(countries.AEs.trans) <- countries.AEs.trans$Name
 countries.AEs.trans.num <- countries.AEs.trans[, 2:ncol(countries.AEs.trans)]
 
-#scale
+#scale data
 countries.AEs.trans.scale <- scale(countries.AEs.trans.num)
+
+#check variable correlations
+cor(countries.AEs.trans.scale)
+
+#dizziness and fatigue, and dizziness and headache are highly correlated so remove dizziness from analysis
+countries.AEs.trans.scale <- countries.AEs.trans.scale[ , !(colnames(countries.AEs.trans.scale) %in% 'index_aes.DIZZINESS')]
 
 #look at options for number of clusters
 set.seed(2017)
@@ -220,7 +229,7 @@ countries.AEs.trans.clus5$region <- rownames(countries.AEs.trans.scale)
 world.aes.clus5 <- merge(x=world, y=countries.AEs.trans.clus5, by="region", all.x=TRUE)
 world.aes.clus5 <- world.aes.clus5[order(world.aes.clus5$order), ]
 
-#plot
+#plot clusters on map
 ggplot(world.aes.clus5, aes(long, lat)) + 
   geom_polygon(aes(fill=fit.cluster, group=group),colour="black",size=0.1) +
   coord_equal() + 
